2698640716bc5f51a61a7b43acc193f3326a509c
Avinash <53809765+avinash7375@users.noreply.github.com>
2022-04-19 23:06:52 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,21 @@
+a=int(input("enter any number"))
+b=int(input("enter any number"))
+c=0
+
+c = a & b;
+print("Line 1 - Value of c is", c)
+
+c = a | b;
+print("Line 2 - Value of c is", c)
+
+c = a ^ b;
+print("Line 3 - Value of c is", c)
+
+c = ~a;
+print("Line 4 - Value of c is", c)
+
+c = a << 2;
+print("Line 5 - Value of c is", c)
+
+c = a >> 2;
+print("Line 5 - Value of c is", c)
